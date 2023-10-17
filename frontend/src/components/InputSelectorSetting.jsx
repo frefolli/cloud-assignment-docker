8a39a920ee3aac126f210026e97161fee05a3f55
q.toska <q.toska@campus.unimib.it>
2023-10-17 15:26:36 +0200

    added other docs

--- frontend/src/components/InputSelectorSetting.jsx
+++ frontend/src/components/InputSelectorSetting.jsx
@@ -2,6 +2,32 @@ import * as React from 'react';
 import JimTable from './JimTable';
 import Selector from './Selector';
 
+/**
+ * This component is responsible for rendering an input selector setting with a title, a selector component, and options.
+ * 
+ * @class InputSelectorSetting
+ * 
+ * @param {Object} props - The component's properties.
+ * @param {string} props.title - The title to be displayed in the centered paragraph.
+ * @param {string} props.label - The label to be displayed for the selector.
+ * @param {string} props.value - The current value of the selector.
+ * @param {Array} props.options - An array of options for the selector.
+ * @param {function} props.onChange - A callback function to be called when the selector value changes.
+ * 
+ * @return {JSX.Element} A component containing a centered paragraph, a selector input field, and options.
+ * 
+ * @example
+ * // Example usage of the InputSelectorSetting component
+ * <InputSelectorSetting
+ *   title="Settings"
+ *   label="Selector Label"
+ *   value={selectorValue}
+ *   options={selectorOptions}
+ *   onChange={handleSelectorChange}
+ * />
+ */
+
+
 export default class InputSelectorSetting extends React.Component {
     render() {
         return (<JimTable>
